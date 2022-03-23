f6e30b62232006dfeb65c5b87919174fc2bb489c
Kanchan Kaushal <kaushalkanchan238@gmail.com>
2022-03-23 13:25:06 +0530

    first commit

---
+++ WebApiDemo/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class StudentController : ApiController
+    {
+        IList<Student> Students = new List<Student>()
+        {
+            new Student()
+                {
+                    StudentId = 1, StudentName = "Kanchan", Address = "Himachal", Course = "IT"
+                },
+                new Student()
+                {
+                    StudentId = 2, StudentName = "Amit", Address = "Delhi", Course = "HR"
+                },
+                new Student()
+                {
+                    StudentId = 3, StudentName = "Rahul", Address = "MP", Course = "IT"
+                },
+                new Student()
+                {
+                    StudentId = 4, StudentName = "Kritika", Address = "Punjab", Course = "Accounts"
+                }
+        };
+        public IList<Student> Get()
+        {  
+            return Students;
+        }
+        public Student Get(int id)
+        { 
+            var Student = Students.FirstOrDefault(e => e.StudentId == id);
+            if (Student == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return Student;
+        
+        }
+    }
+}
---
+++ WebApiDemo/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo.Models
+{
+    public class Student
+    {
+        public int StudentId
+        {
+            get;
+            set;
+        }
+        public string StudentName
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string Course
+        {
+            get;
+            set;
+        }
+    }
+}
